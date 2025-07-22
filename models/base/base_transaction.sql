c2a2cb40d91e1c2e642a8bd77a340ce36bc70acc
Jaime Colmenares <jaime.colmenares@gmail.com>
2025-07-22 15:39:07 +0000

    Updated format of base_transaction.sql

--- models/base/base_transaction.sql
+++ models/base/base_transaction.sql
@@ -1,30 +1,30 @@
-SELECT
-  _file
-  , _line
-  , _modified AS _modified_ts
-  , _fivetran_synced AS _fivetran_synced_ts
-  , CURRENT_TIMESTAMP AS _base_updated_ts
-  , row_id AS _source_row_id
-  , customer_id
-  , customer_name
-  , segment
-  , city
-  , state
-  , postal_code
-  , country
-  , market
-  , region
-  , product_id
-  , product_name
-  , category
-  , sub_category
-  , order_id
-  , order_date
-  , order_priority
-  , ship_date
-  , ship_mode
-  , quantity
-  , sales
-  , discount
-  , profit
-FROM {{ source('google_drive', 'transactions') }}
+select
+    _file,
+    _line,
+    _modified as _modified_ts,
+    _fivetran_synced as _fivetran_synced_ts,
+    current_timestamp as _base_updated_ts,
+    row_id as _source_row_id,
+    customer_id,
+    customer_name,
+    segment,
+    city,
+    state,
+    postal_code,
+    country,
+    market,
+    region,
+    product_id,
+    product_name,
+    category,
+    sub_category,
+    order_id,
+    order_date,
+    order_priority,
+    ship_date,
+    ship_mode,
+    quantity,
+    sales,
+    discount,
+    profit
+from {{ source("google_drive", "transactions") }}
